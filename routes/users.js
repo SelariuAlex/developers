741ee93c618d406d31b4e832e3ea1c1fa8de6ab3
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-21 11:30:50 +0300

    validate register user using express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// GET api/users
-router.get('/', (req, res) => res.send('Users'));
+// POST api/users
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 3 or more characters'
+    ).isLength({ min: 3 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('Users');
+  }
+);
 
 module.exports = router;
