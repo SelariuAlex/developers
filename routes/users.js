d7ac209b79cbdfc59eafad49308d4bb17c3147d3
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-21 17:44:10 +0300

    send token with JwT

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -60,7 +62,22 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
+      // return jwt
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server error');
