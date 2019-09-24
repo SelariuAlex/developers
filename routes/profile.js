027b099eeee18f47aa2690644e6e270f9bccdfc0
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-24 10:55:45 +0300

    get all profiles and get profile by id

--- routes/profile.js
+++ routes/profile.js
@@ -105,4 +105,34 @@ router.post(
   }
 );
 
+// GET api/profile
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// GET api/profile/user/:user_id
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
