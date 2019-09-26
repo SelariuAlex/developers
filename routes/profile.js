06dabb1a2bfcb8e22bf8a68697bfa0ddcf6c1a51
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-26 11:40:41 +0300

    get github repos

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const request = require('request');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const Profile = require('../models/Profile');
@@ -310,6 +312,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+// GET api/profile/github/:username
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=6&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
