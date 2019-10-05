44856dad4fd52bbcc9507c9a56d562fb4707b4fd
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-10-05 11:18:55 +0300

    create private routes

--- client/src/components/routes/Routes.component.js
+++ client/src/components/routes/Routes.component.js
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import Login from '../auth/Login/Login.component';
 import Register from '../auth/Register/Register.component';
 import Alert from '../layout/Alert/Alert.component';
+import Dashboard from '../dashboard/Dashboard.component';
+import PrivateRoute from '../routes/PrivateRoute.component';
 import NotFound from '../layout/NotFound/NotFound.component';
 
 const Routes = () => {
@@ -12,6 +14,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
       <Alert />
