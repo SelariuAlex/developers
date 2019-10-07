fb5728f67cc0227022c9d5791ddf86b5d8d0a767
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-10-07 18:52:06 +0300

    Create profile component

--- client/src/components/routes/Routes.component.js
+++ client/src/components/routes/Routes.component.js
@@ -6,6 +6,7 @@ import Register from '../auth/Register/Register.component';
 import Alert from '../layout/Alert/Alert.component';
 import Dashboard from '../dashboard/Dashboard.component';
 import PrivateRoute from '../routes/PrivateRoute.component';
+import CreateProfile from '../profile-form/CreateProfile.component';
 import NotFound from '../layout/NotFound/NotFound.component';
 
 const Routes = () => {
@@ -15,6 +16,8 @@ const Routes = () => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+        <Route component={NotFound} />
         <Route component={NotFound} />
       </Switch>
       <Alert />
