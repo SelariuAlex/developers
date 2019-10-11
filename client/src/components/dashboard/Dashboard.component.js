4f8e54353bf3f2aa5344b0f985d39c19095e24af
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-10-11 16:14:09 +0300

    edit profile component

--- client/src/components/dashboard/Dashboard.component.js
+++ client/src/components/dashboard/Dashboard.component.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../redux/actions/profile.action';
+import DashboardActions from './DashboardActions.component';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -23,7 +24,9 @@ const Dashboard = ({
         <i className="fas fa-user" /> Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile</p>
--- client/src/components/routes/Routes.component.js
+++ client/src/components/routes/Routes.component.js
@@ -7,6 +7,7 @@ import Alert from '../layout/Alert/Alert.component';
 import Dashboard from '../dashboard/Dashboard.component';
 import PrivateRoute from '../routes/PrivateRoute.component';
 import CreateProfile from '../profile-form/CreateProfile.component';
+import EditProfile from '../profile-form/EditProfile.component';
 import NotFound from '../layout/NotFound/NotFound.component';
 
 const Routes = () => {
@@ -17,6 +18,7 @@ const Routes = () => {
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+        <PrivateRoute exact path="/edit-profile" component={EditProfile} />
         <Route component={NotFound} />
         <Route component={NotFound} />
       </Switch>
