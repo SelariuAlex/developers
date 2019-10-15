4627bc6ef48276e7cfbb35f6756de185d753af20
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-10-15 11:54:07 +0300

    Get profiles get repos action and reducer

--- client/src/redux/types/types.js
+++ client/src/redux/types/types.js
@@ -15,7 +15,9 @@ export const LOGOUT = 'LOGOUT';
 
 // Profile
 export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
 export const PROFILE_ERROR = 'PROFIL_ERROR';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
+export const GET_REPOS = 'GET_REPOS';
