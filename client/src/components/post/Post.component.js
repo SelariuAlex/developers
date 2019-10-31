926cd953b50d1dfa12883551bc1cf77ec0e66dfe
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-10-31 12:51:09 +0200

    comment item and delete comment

--- client/src/components/post/Post.component.js
+++ client/src/components/post/Post.component.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner/Spinner.component';
 import PostItem from '../posts/PostItem.component';
 import CommentForm from './CommentForm.component';
+import CommentItem from './CommentItem.component';
 
 import { getPost } from '../../redux/actions/post.action';
 
@@ -22,6 +23,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
